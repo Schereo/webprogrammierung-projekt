bd563670ec34721b8e6d52b3c5055418cda21a86
euer Name <tim.sigl@uni-oldenburg.de>
2019-04-06 15:16:08 +0200

    init commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!doctype html>
+
+<html lang="de">
+    <head>
+        <meta charset="utf-8">
+        <title>Webprogrammierung Projekt</title>
+    </head>
+
+    <body>
+    <?php include ('header.php'); ?>
+    <h1>Main-Content</h1>
+    <?php include ('footer.php'); ?>
+    </body>
+</html>
